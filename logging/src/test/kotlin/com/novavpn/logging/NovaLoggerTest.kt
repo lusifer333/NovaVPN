973927c757dcd20a7077152aeed85647a5fe444b
Hermes Builder <hermes@novavpn.dev>
2026-07-29 12:53:27 +0000

    fix: add missing coroutine imports in NovaLoggerTest

--- logging/src/test/kotlin/com/novavpn/logging/NovaLoggerTest.kt
+++ logging/src/test/kotlin/com/novavpn/logging/NovaLoggerTest.kt
@@ -2,6 +2,8 @@ package com.novavpn.logging
 
 import com.novavpn.domain.model.LogLevel
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Test
